1670fafe37e13e7c417abf5f6435f751e8060d23
Wills <ejioguvictor6@gmail.com>
2022-10-26 15:06:48 +0100

    content of the home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -186,6 +186,160 @@ const Home = () => {
             Gubelin, SSCF, GRS, GIA
           </p>
         </div>
+        <div className="quote">
+          <p>
+            {" "}
+            'Whether it is an amethyst or a ruby, one thing is certain that they
+            will both be of great quality.'
+          </p>
+        </div>
+        <div className="quote-img-ctn">
+          <div className="quote-img-one"></div>
+          <div className="quote-img-two"></div>
+        </div>
+        <div className="about-us-ctn">
+          <Link to="/what-we-do">
+            <div className="about-us">What we do</div>
+          </Link>
+        </div>
+        <div className="title-ctn">
+          <h2 className="first-title">GEMSTONE SPECIALISTS</h2>
+          <h1 className="second-title">
+            We specialise in a wide variety of coloured gemstones and diamonds
+          </h1>
+        </div>
+        <div className="journey-ctn">
+          <div className="journey">
+            <div className="journey-ima-ctn image-one"></div>
+            <div className="journey-wrapper">
+              <div className="journey-paragraph-ctn">
+                <h4 className="journey-country">Our Expertise</h4>
+                <p className="journey-paragraph">
+                  We specialise in a wide variety of fine coloured gemstones and
+                  coloured diamonds. Through our extensive worldwide network of
+                  contacts we are able to source gemstones from all over the
+                  world at the best possible prices.
+                </p>
+                <p className="journey-paragraph">
+                  We always strive to listen to our clients needs and
+                  specifications and aim to offer suitable solutions that meet
+                  with their expectations.
+                </p>
+                <p className="journey-paragraph">
+                  We frequently offer our clients our expert advice on coloured
+                  gemstones discussing with their jewellery designers and
+                  manufacturers the availability price and supply of their
+                  chosen gemstone.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="journey ">
+            <div className="journey-wrapper">
+              <div className="journey-paragraph-ctn">
+                <h4 className="journey-country">Passion for Gemstones</h4>
+                <p className="journey-paragraph">
+                  It's an exciting process and certainly very rewarding to see
+                  our gems being used by some of the best jewellery designers
+                  and jewellery houses in the world.
+                </p>
+                <p className="journey-paragraph">
+                  Ranging from ruby, sapphire, emerald, opal, aquamarine,
+                  tsavorite, tourmaline, rubellite, fine moonstone to pink
+                  diamonds and other gemstones, our main aim is to supply
+                  gemstones of fine quality.
+                </p>
+                <p className="journey-paragraph">
+                  Whether it is an amethyst or a ruby, one thing is certain that
+                  they will both be of exceptional quality.
+                </p>
+              </div>
+            </div>
+
+            <div className="journey-ima-ctn image-two"></div>
+          </div>
+        </div>
+        <div className="about-us-ctn">
+          <Link to="/gemstones">
+            <div className="about-us">Gemstones</div>
+          </Link>
+        </div>
+        <div className="title-ctn">
+          <h2 className="first-title">FINE JEWELLERY</h2>
+          <h1 className="second-title">
+            One-of-a-kind Bespoke jewellery Antique Jewellery
+          </h1>
+        </div>
+        <div className="journey-ctn">
+          <div className="journey">
+            <div className="journey-ima-ctn image-one"></div>
+            <div className="journey-wrapper">
+              <div className="journey-paragraph-ctn">
+                <h4 className="journey-country">Unique Bespoke Jewellery</h4>
+                <p className="journey-paragraph">
+                  Our many years of expertise in the jewellery industry has
+                  enabled us to develop and build trusted relationships with our
+                  very own private clients, of whom many we are now fortunate
+                  enough to call our friends and who frequently call and
+                  commission us to manufacture for them and their families
+                  unique bespoke jewellery.
+                </p>
+                <h4 className="journey-country">
+                  The Jewellery Design Process
+                </h4>
+                <p className="journey-paragraph">
+                  Our approach to designing jewellery has always been to firstly
+                  listen carefully to the client’s desires and requirements and
+                  then guide them through the process providing them with
+                  options that suit their budget, taste and choice of design.
+                  This means that we explore gemstone options, designs,
+                  manufacturing materials and methods, always allowing the
+                  client to make informed decisions about their chosen design.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="journey ">
+            <div className="journey-wrapper">
+              <div className="journey-paragraph-ctn">
+                <h4 className="journey-country">
+                  Exceptional Quality and Craftsmanship
+                </h4>
+                <p className="journey-paragraph">
+                  Once designs have been approved, we manufacture their chosen
+                  piece using some of the best jewellery manufacturers in the
+                  UK. We are really passionate about high quality manufacturing
+                  with attention to detail and our clients value our expert
+                  advice and skills allowing them to feel confident about the
+                  whole process.
+                </p>
+                <h4 className="journey-country">
+                  The Uniqueness of Personalised Design
+                </h4>
+                <p className="journey-paragraph">
+                  Designing a bespoke piece of jewellery, whether it is an
+                  engagement ring, an anniversary gift or any other occasion, is
+                  a really unique and exciting process to go through, as it
+                  allows clients to be involved in the process and gives them
+                  the opportunity to add their very own touch of
+                  personalisation.
+                </p>
+                <p className="journey-paragraph">
+                  We take great pride in our jewellery and the trust our clients
+                  place in us and believe that each piece of jewellery designed
+                  and manufactured should enhance the wearers’ experience and
+                  satisfaction.
+                </p>
+              </div>
+            </div>
+            <div className="journey-ima-ctn image-two"></div>
+          </div>
+        </div>
+        <div className="about-us-ctn">
+          <Link to="/jewelries">
+            <div className="about-us">Browse...</div>
+          </Link>
+        </div>
       </div>
     </div>
   );
