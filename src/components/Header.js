a358ac9adaafa1c2eea4dc52ae8b19caee90c1e3
Wills <ejioguvictor6@gmail.com>
2022-10-27 10:29:37 +0100

    content of the home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/images/wills (1).png";
 
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header-ctn">
-      <div className="logo-ctn">
-        <Link to="/">Our Logo</Link>
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
       </div>
       <div className="nav-ctn">
         <Link to="/">Home</Link> |<Link to="about-us">About us</Link> |
