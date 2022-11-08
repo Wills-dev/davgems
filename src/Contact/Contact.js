0be98a19fb1c4deea17ca60176ed267f02c370e4
Wills <ejioguvictor6@gmail.com>
2022-11-08 09:54:41 +0100

    added phone icon to mobile view

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,11 +2,13 @@ import React from "react";
 import ContactCom from "../components/ContactCom";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import PhoneNumber from "../components/PhoneNumber";
 
 const Contact = () => {
   return (
     <div className="main">
       <Header />
+      <PhoneNumber />
       <NavBar />
       <div className="main-body">
         <ContactCom />
